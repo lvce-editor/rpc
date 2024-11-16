e57ab521e04ed9c72f00b0425d918b0e318944ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 19:29:03 +0100

    feature: add webworker rpc client (#4)

---
+++ src/parts/JsonRpc/JsonRpc.ts
@@ -0,0 +1,1 @@
+export { handleJsonRpcMessage, invoke, resolve } from '@lvce-editor/json-rpc'
---
+++ src/parts/WebWorkerRpcClient/WebWorkerRpcClient.ts
@@ -0,0 +1,17 @@
+import * as Command from '../Command/Command.ts'
+import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+import * as IpcChild from '../IpcChild/IpcChild.ts'
+
+export const create = async ({
+  commandMap,
+}: {
+  commandMap: any
+}): Promise<any> => {
+  // TODO create a commandMap per rpc instance
+  Command.register(commandMap)
+  const ipc = await IpcChild.listen()
+  HandleIpc.handleIpc(ipc)
+  const rpc = CreateRpc.createRpc(ipc)
+  return rpc
+}
