596a316a5db5cdaa607a752c1a873d26a08ca068
Le Vivilet <levivlet@pm.me>
2024-11-17 15:44:40 +0100

    feature: add rpc send method

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,8 @@
 export interface Rpc {
+  readonly send: (method: string, ...params: any[]) => void
+
   readonly invoke: (method: string, ...params: any[]) => Promise<any>
+
   readonly invokeAndTransfer: (method: string, ...params: any[]) => Promise<any>
 }
 
