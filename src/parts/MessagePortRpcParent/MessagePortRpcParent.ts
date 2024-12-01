4e82b75cc94ba0115f12bb628c778d94171efe24
Le Vivilet <levivlet@pm.me>
2024-12-01 18:23:10 +0100

    feature: add isMessagePortOpen option to messageport rpc

--- src/parts/MessagePortRpcParent/MessagePortRpcParent.ts
+++ src/parts/MessagePortRpcParent/MessagePortRpcParent.ts
@@ -7,14 +7,17 @@ import { IpcParentWithMessagePort } from '@lvce-editor/ipc/dist/browser.js'
 export const create = async ({
   commandMap,
   messagePort,
+  isMessagePortOpen,
 }: {
   commandMap: any
   messagePort: MessagePort
+  isMessagePortOpen?: boolean
 }): Promise<any> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
   const rawIpc = await IpcParentWithMessagePort.create({
     messagePort,
+    isMessagePortOpen,
   })
   const ipc = IpcParentWithMessagePort.wrap(rawIpc)
   HandleIpc.handleIpc(ipc)
