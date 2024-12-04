d08f3bc6a274a1f4935d86e5b47859333bfcae39
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 19:05:49 +0100

    feature: pass messagePort to electronMessagePortRpcClient (#33)

--- src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
+++ src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
@@ -4,10 +4,10 @@ import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
 
-export const create = async ({ commandMap }: { commandMap: any }): Promise<any> => {
+export const create = async ({ commandMap, messagePort }: { commandMap: any; messagePort: any }): Promise<any> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
-  const ipc = await IpcChild.listen(IpcChildWithElectronMessagePort)
+  const ipc = await IpcChild.listen(IpcChildWithElectronMessagePort, { messagePort })
   HandleIpc.handleIpc(ipc)
   const rpc = CreateRpc.createRpc(ipc)
   return rpc
