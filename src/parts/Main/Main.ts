3a3bc0f9226c2606c28f833c75ee7592e6de3292
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 18:27:24 +0100

    feature: add nodeWorkerRpcClient (#24)

--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,3 +1,4 @@
 export * as MessagePortRpcParent from '../MessagePortRpcParent/MessagePortRpcParent.ts'
+export * as NodeWorkerRpcClient from '../NodeWorkerRpcClient/NodeWorkerRpcClient.ts'
 export * as WebSocketRpcParent from '../WebSocketRpcParent/WebSocketRpcParent.ts'
 export * as WebWorkerRpcClient from '../WebWorkerRpcClient/WebWorkerRpcClient.ts'
---
+++ src/parts/NodeWorkerRpcClient/NodeWorkerRpcClient.ts
@@ -0,0 +1,18 @@
+import { IpcChildWithNodeWorker } from '@lvce-editor/ipc'
+import * as Command from '../Command/Command.ts'
+import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
+
+export const create = async ({
+  commandMap,
+}: {
+  commandMap: any
+}): Promise<any> => {
+  // TODO create a commandMap per rpc instance
+  Command.register(commandMap)
+  const ipc = await IpcChild.listen(IpcChildWithNodeWorker)
+  HandleIpc.handleIpc(ipc)
+  const rpc = CreateRpc.createRpc(ipc)
+  return rpc
+}
