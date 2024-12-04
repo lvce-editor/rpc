a6a9961042e37595483f7fd3c158f1ffd4d92b90
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-04 19:01:06 +0100

    feature: add electronMessagePort rpc (#31)

---
+++ src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
@@ -0,0 +1,14 @@
+import { IpcChildWithElectronMessagePort } from '@lvce-editor/ipc'
+import * as Command from '../Command/Command.ts'
+import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
+
+export const create = async ({ commandMap }: { commandMap: any }): Promise<any> => {
+  // TODO create a commandMap per rpc instance
+  Command.register(commandMap)
+  const ipc = await IpcChild.listen(IpcChildWithElectronMessagePort)
+  HandleIpc.handleIpc(ipc)
+  const rpc = CreateRpc.createRpc(ipc)
+  return rpc
+}
