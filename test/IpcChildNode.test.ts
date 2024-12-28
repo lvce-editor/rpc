49b592d7a8514d7ddf9c064a742a6ff23f46edaa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-28 22:20:14 +0100

    feature: add ipcchildNode test (#62)
    * feature: add ipcchildNode test
    
    * lint
    
    * test
    
    * lint

---
+++ test/IpcChildNode.test.ts
@@ -0,0 +1,21 @@
+import { expect, jest, test } from '@jest/globals'
+import { listen } from '../src/parts/IpcChildNode/IpcChildNode.js'
+
+test('listen - creates ipc child with given transport and options', async () => {
+  const rawIpc = {}
+  const wrappedIpc = {}
+  const module = {
+    listen: jest.fn(() => {
+      return rawIpc
+    }),
+    signal(): void {},
+    wrap(): any {
+      return wrappedIpc
+    },
+  }
+  const options = {}
+  const result = await listen(module, options)
+
+  expect(module.listen).toHaveBeenCalledWith(options)
+  expect(result).toBe(wrappedIpc)
+})
