bbe35acad5f7447e9c29988bcb317c854a3511bc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-28 22:17:25 +0100

    Discard changes to src/parts/NodeWorkerRpcClient/NodeWorkerRpcClient.ts

--- src/parts/NodeWorkerRpcClient/NodeWorkerRpcClient.ts
+++ src/parts/NodeWorkerRpcClient/NodeWorkerRpcClient.ts
@@ -3,9 +3,12 @@ import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
-import { Rpc } from '../Rpc/Rpc.ts'
 
-export const create = async ({ commandMap }: { commandMap: any }): Promise<Rpc> => {
+export const create = async ({
+  commandMap,
+}: {
+  commandMap: any
+}): Promise<any> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
   const ipc = await IpcChild.listen(IpcChildWithNodeWorker)
