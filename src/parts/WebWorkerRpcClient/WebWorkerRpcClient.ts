d597700baaaf16f06b6af79e66fc83dba59ebaa7
Le Vivilet <levivlet@pm.me>
2024-12-28 22:15:44 +0100

    feature: sort imports

--- src/parts/WebWorkerRpcClient/WebWorkerRpcClient.ts
+++ src/parts/WebWorkerRpcClient/WebWorkerRpcClient.ts
@@ -1,8 +1,8 @@
+import { IpcChildWithModuleWorkerAndMessagePort } from '@lvce-editor/ipc'
 import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
-import { IpcChildWithModuleWorkerAndMessagePort } from '@lvce-editor/ipc'
 
 export const create = async ({ commandMap }: { commandMap: any }): Promise<any> => {
   // TODO create a commandMap per rpc instance
