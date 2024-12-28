9d7966823cc4b77da0c0f6625311fa7fc67dd1fe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-28 22:17:06 +0100

    Discard changes to src/parts/MessagePortRpcParent/MessagePortRpcParent.ts

--- src/parts/MessagePortRpcParent/MessagePortRpcParent.ts
+++ src/parts/MessagePortRpcParent/MessagePortRpcParent.ts
@@ -2,7 +2,6 @@ import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import { IpcParentWithMessagePort } from '@lvce-editor/ipc'
-import { Rpc } from '../Rpc/Rpc.ts'
 
 export const create = async ({
   commandMap,
@@ -12,7 +11,7 @@ export const create = async ({
   commandMap: any
   messagePort: MessagePort
   isMessagePortOpen?: boolean
-}): Promise<Rpc> => {
+}): Promise<any> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
   const rawIpc = await IpcParentWithMessagePort.create({
