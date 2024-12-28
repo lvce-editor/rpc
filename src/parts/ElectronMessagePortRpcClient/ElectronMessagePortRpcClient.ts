db9bbc78d61f83f9d74aefd55bd49f49465dd8ee
Le Vivilet <levivlet@pm.me>
2024-12-28 22:20:49 +0100

    lint

--- src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
+++ src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
@@ -1,9 +1,9 @@
 import { IpcChildWithElectronMessagePort } from '@lvce-editor/ipc'
+import type { Rpc } from '../Rpc/Rpc.ts'
 import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
-import type { Rpc } from '../Rpc/Rpc.ts'
 
 export const create = async ({ commandMap, messagePort }: { commandMap: any; messagePort: any }): Promise<Rpc> => {
   // TODO create a commandMap per rpc instance
--- src/parts/NodeWorkerRpcClient/NodeWorkerRpcClient.ts
+++ src/parts/NodeWorkerRpcClient/NodeWorkerRpcClient.ts
@@ -1,9 +1,9 @@
 import { IpcChildWithNodeWorker } from '@lvce-editor/ipc'
+import type { Rpc } from '../Rpc/Rpc.ts'
 import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
-import type { Rpc } from '../Rpc/Rpc.ts'
 
 export const create = async ({ commandMap }: { commandMap: any }): Promise<Rpc> => {
   // TODO create a commandMap per rpc instance
