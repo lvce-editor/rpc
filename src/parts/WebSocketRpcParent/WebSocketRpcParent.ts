0f2ac236f578c87bc353bbaa3a6d34bc771cd42c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-28 22:17:29 +0100

    Discard changes to src/parts/WebSocketRpcParent/WebSocketRpcParent.ts

--- src/parts/WebSocketRpcParent/WebSocketRpcParent.ts
+++ src/parts/WebSocketRpcParent/WebSocketRpcParent.ts
@@ -2,9 +2,8 @@ import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import { IpcParentWithWebSocket } from '@lvce-editor/ipc'
-import { Rpc } from '../Rpc/Rpc.ts'
 
-export const create = async ({ commandMap, webSocket }: { commandMap: any; webSocket: WebSocket }): Promise<Rpc> => {
+export const create = async ({ commandMap, webSocket }: { commandMap: any; webSocket: WebSocket }): Promise<any> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
   const rawIpc = await IpcParentWithWebSocket.create({
