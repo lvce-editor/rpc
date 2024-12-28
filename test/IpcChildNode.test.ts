227d4b65a069ed713a74b66ede96742625a5dc74
Le Vivilet <levivlet@pm.me>
2024-12-28 22:09:31 +0100

    test

--- test/IpcChildNode.test.ts
+++ test/IpcChildNode.test.ts
@@ -1,51 +1,21 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
-
-const mockIpcChildWithWebSocket = {
-  create: jest.fn(),
-}
-
-beforeEach(() => {
-  mockIpcChildWithWebSocket.create.mockClear()
-})
-
-jest.unstable_mockModule('@lvce-editor/ipc', () => ({
-  IpcChildWithWebSocket: mockIpcChildWithWebSocket,
-}))
-
-const { listen } = await import('../src/parts/IpcChildNode/IpcChildNode.js')
+import { expect, jest, test } from '@jest/globals'
+import { listen } from '../src/parts/IpcChildNode/IpcChildNode.js'
 
 test('listen - creates ipc child with given transport and options', async () => {
-  const mockIpc = {
-    send: jest.fn(),
-    invoke: jest.fn(),
+  const rawIpc = {}
+  const wrappedIpc = {}
+  const module = {
+    listen: jest.fn(() => {
+      return rawIpc
+    }),
+    signal() {},
+    wrap() {
+      return wrappedIpc
+    },
   }
-  const mockTransport = mockIpcChildWithWebSocket
-  const mockOptions = {
-    request: {},
-    handle: {},
-  }
+  const options = {}
+  const result = await listen(module, options)
 
-  // @ts-ignore
-  mockIpcChildWithWebSocket.create.mockResolvedValue(mockIpc)
-
-  const result = await listen(mockTransport, mockOptions)
-
-  expect(mockTransport.create).toHaveBeenCalledWith(mockOptions)
-  expect(result).toBe(mockIpc)
-})
-
-test('listen - creates ipc child with transport and no options', async () => {
-  const mockIpc = {
-    send: jest.fn(),
-    invoke: jest.fn(),
-  }
-  const mockTransport = mockIpcChildWithWebSocket
-
-  // @ts-ignore
-  mockIpcChildWithWebSocket.create.mockResolvedValue(mockIpc)
-
-  const result = await listen(mockTransport)
-
-  expect(mockTransport.create).toHaveBeenCalledWith(undefined)
-  expect(result).toBe(mockIpc)
+  expect(module.listen).toHaveBeenCalledWith(options)
+  expect(result).toBe(wrappedIpc)
 })
