712bd64615d4a7a258c3880bbc2a77936810cca9
Le Vivilet <levivlet@pm.me>
2024-12-28 22:20:19 +0100

    fix type

--- src/parts/Rpc/Rpc.ts
+++ src/parts/Rpc/Rpc.ts
@@ -1,5 +1,5 @@
 export interface Rpc {
   readonly send: (method: string, ...params: any[]) => void
-  readonly invoke: (method: string, ...params: any[]) => Promise<void>
-  readonly invokeAndTransfer: (method: string, ...params: any[]) => Promise<void>
+  readonly invoke: (method: string, ...params: any[]) => Promise<any>
+  readonly invokeAndTransfer: (method: string, ...params: any[]) => Promise<any>
 }
