7a3e769b46e6d5e3dbc6f70dc2548d95e96f9fe5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-28 22:16:59 +0100

    Discard changes to src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts

--- src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
+++ src/parts/ElectronMessagePortRpcClient/ElectronMessagePortRpcClient.ts
@@ -3,9 +3,8 @@ import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcChild from '../IpcChildNode/IpcChildNode.ts'
-import { Rpc } from '../Rpc/Rpc.ts'
 
-export const create = async ({ commandMap, messagePort }: { commandMap: any; messagePort: any }): Promise<Rpc> => {
+export const create = async ({ commandMap, messagePort }: { commandMap: any; messagePort: any }): Promise<any> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
   const ipc = await IpcChild.listen(IpcChildWithElectronMessagePort, { messagePort })
