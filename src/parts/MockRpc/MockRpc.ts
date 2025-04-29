2aebde98cabf68f69a7c71e503741b9e1f736faf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-29 22:46:44 +0200

    feature: allow passing invokeAndTransfer function to createmockRpc (#108)

--- src/parts/MockRpc/MockRpc.ts
+++ src/parts/MockRpc/MockRpc.ts
@@ -1,8 +1,9 @@
 import type { Rpc } from '../Rpc/Rpc.ts'
 
-export const create = ({ invoke }: { invoke: any }): Rpc => {
+export const create = ({ invoke, invokeAndTransfer }: { invoke: any; invokeAndTransfer?: any }): Rpc => {
   const mockRpc = {
     invoke,
+    invokeAndTransfer,
   } as any
   return mockRpc
 }
