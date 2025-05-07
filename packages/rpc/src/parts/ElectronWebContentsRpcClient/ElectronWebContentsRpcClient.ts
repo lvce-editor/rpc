f4d3483e283b952e1799644b44822ddb2fe6133d
Le Vivilet <levivlet@pm.me>
2025-05-07 22:35:42 +0200

    feature: add electron webcontents rpc

---
+++ packages/rpc/src/parts/ElectronWebContentsRpcClient/ElectronWebContentsRpcClient.ts
@@ -0,0 +1,28 @@
+import { IpcChildWithRendererProcess2 } from '@lvce-editor/ipc'
+import type { Rpc } from '../Rpc/Rpc.ts'
+import * as Command from '../Command/Command.ts'
+import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+
+export const create = async ({
+  commandMap,
+  webContents,
+  requiresSocket,
+}: {
+  commandMap: any
+  webContents: any
+  requiresSocket?: any
+}): Promise<Rpc> => {
+  // TODO create a commandMap per rpc instance
+  Command.register(commandMap)
+  const ipc = await IpcChildWithRendererProcess2.listen({
+    webContents,
+  })
+  if (requiresSocket) {
+    // @ts-ignore
+    ipc.requiresSocket = requiresSocket
+  }
+  HandleIpc.handleIpc(ipc)
+  const rpc = CreateRpc.createRpc(ipc)
+  return rpc
+}
