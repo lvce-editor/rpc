41da3a0626558a9b1b56535b395e7cd51c542b02
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 18:02:24 +0200

    feature: make websocket type configurable (#165)

--- packages/rpc/src/parts/WebSocketRpcParent2/WebSocketRpcParent2.ts
+++ packages/rpc/src/parts/WebSocketRpcParent2/WebSocketRpcParent2.ts
@@ -3,10 +3,10 @@ import * as GetWebSocketUrl from '../GetWebSocketUrl/GetWebSocketUrl.ts'
 import * as Location from '../Location/Location.ts'
 import * as WebSocketRpcParent from '../WebSocketRpcParent/WebSocketRpcParent.ts'
 
-export const create = async ({ commandMap }: { commandMap: any }): Promise<Rpc> => {
+export const create = async ({ commandMap, type }: { commandMap: any; type: string }): Promise<Rpc> => {
   const host = Location.getHost()
   const protocol = Location.getProtocol()
-  const wsUrl = GetWebSocketUrl.getWebSocketUrl('search-process', host, protocol)
+  const wsUrl = GetWebSocketUrl.getWebSocketUrl(type, host, protocol)
   const webSocket = new WebSocket(wsUrl)
   const rpc = await WebSocketRpcParent.create({
     webSocket,
