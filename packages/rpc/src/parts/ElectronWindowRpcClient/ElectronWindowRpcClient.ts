99c28330c09c793c26209a4b443ed75c3615cd22
Le Vivilet <levivlet@pm.me>
2025-07-08 07:41:16 +0200

    lint

--- packages/rpc/src/parts/ElectronWindowRpcClient/ElectronWindowRpcClient.ts
+++ packages/rpc/src/parts/ElectronWindowRpcClient/ElectronWindowRpcClient.ts
@@ -1,8 +1,8 @@
 import { IpcChildWithElectronWindow } from '@lvce-editor/ipc'
+import type { Rpc } from '../Rpc/Rpc.ts'
 import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
-import type { Rpc } from '../Rpc/Rpc.ts'
 
 export const create = async ({ commandMap, window }: { commandMap: any; window: any }): Promise<Rpc> => {
   // TODO create a commandMap per rpc instance
