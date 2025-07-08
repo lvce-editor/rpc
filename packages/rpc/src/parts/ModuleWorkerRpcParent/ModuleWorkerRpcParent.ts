1250434ea72cfa1a660f91ef901be8cac5cb51bf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-08 14:58:32 +0200

    feature: add module worker rpc (#177)

---
+++ packages/rpc/src/parts/ModuleWorkerRpcParent/ModuleWorkerRpcParent.ts
@@ -0,0 +1,30 @@
+import { IpcParentWithModuleWorker } from '@lvce-editor/ipc'
+import type { Rpc } from '../Rpc/Rpc.ts'
+import * as Command from '../Command/Command.ts'
+import { createRpc } from '../CreateRpc/CreateRpc.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
+import * as IsWorker from '../IsWorker/IsWorker.ts'
+
+export const create = async ({
+  commandMap,
+  url,
+  name,
+}: {
+  commandMap: any
+  url: string
+  name?: string
+}): Promise<Rpc> => {
+  // TODO create a commandMap per rpc instance
+  Command.register(commandMap)
+  const worker = await IpcParentWithModuleWorker.create({
+    url,
+    name,
+  })
+  if (!IsWorker.isWorker(worker)) {
+    throw new Error(`worker must be of type Worker`)
+  }
+  const ipc = IpcParentWithModuleWorker.wrap(worker)
+  HandleIpc.handleIpc(ipc)
+  const workerRpc = createRpc(ipc)
+  return workerRpc
+}
