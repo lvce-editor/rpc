7569710a291959915ef0af922e76f8e22909962c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-05 12:58:13 +0200

    feature: add stdout and stderr options to node worker rpc (#188)

--- packages/rpc/src/parts/NodeWorkerRpcParent/NodeWorkerRpcParent.ts
+++ packages/rpc/src/parts/NodeWorkerRpcParent/NodeWorkerRpcParent.ts
@@ -1,5 +1,5 @@
 import { IpcParentWithNodeWorker } from '@lvce-editor/ipc'
-import type { Rpc } from '../Rpc/Rpc.ts'
+import type { RpcWithStdio } from '../RpcWithStdio/RpcWithStdio.ts'
 import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
@@ -15,11 +15,10 @@ export const create = async ({
   commandMap: any
   argv?: readonly string[]
   execArgv?: readonly string[]
-  isMessagePortOpen?: boolean
   path: string
   stdio?: string
   env?: any
-}): Promise<Rpc> => {
+}): Promise<RpcWithStdio> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
   const rawIpc = await IpcParentWithNodeWorker.create({
@@ -32,5 +31,10 @@ export const create = async ({
   const ipc = IpcParentWithNodeWorker.wrap(rawIpc)
   HandleIpc.handleIpc(ipc)
   const rpc = CreateRpc.createRpc(ipc)
+  // @ts-ignore
+  rpc.stdout = ipc._rawIpc.stdout
+  // @ts-ignore
+  rpc.stderr = ipc._rawIpc.stderr
+  // @ts-ignore
   return rpc
 }
