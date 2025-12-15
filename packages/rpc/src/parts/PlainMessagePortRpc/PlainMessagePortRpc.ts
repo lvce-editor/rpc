df18d01019ebd86e40e428b895dfe4f990370ab6
Le Vivilet <levivilet@pm.me>
2025-12-15 22:17:36 +0100

    feature: allow marking message port as already open or not

--- packages/rpc/src/parts/PlainMessagePortRpc/PlainMessagePortRpc.ts
+++ packages/rpc/src/parts/PlainMessagePortRpc/PlainMessagePortRpc.ts
@@ -1,25 +1,27 @@
-import { IpcParentWithMessagePort } from "@lvce-editor/ipc";
-import type { Rpc } from "../Rpc/Rpc.ts";
-import * as Command from "../Command/Command.ts";
-import * as CreateRpc from "../CreateRpc/CreateRpc.ts";
-import * as HandleIpc from "../HandleIpc/HandleIpc.ts";
+import { IpcParentWithMessagePort } from '@lvce-editor/ipc'
+import type { Rpc } from '../Rpc/Rpc.ts'
+import * as Command from '../Command/Command.ts'
+import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
+import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 
 export const create = async ({
   commandMap,
   messagePort,
+  isMessagePortOpen = true,
 }: {
-  commandMap: any;
-  messagePort: MessagePort;
+  commandMap: any
+  messagePort: MessagePort
+  isMessagePortOpen?: boolean
 }): Promise<Rpc> => {
   // TODO create a commandMap per rpc instance
-  Command.register(commandMap);
+  Command.register(commandMap)
   const rawIpc = await IpcParentWithMessagePort.create({
-    isMessagePortOpen: true,
+    isMessagePortOpen,
     messagePort,
-  });
-  const ipc = IpcParentWithMessagePort.wrap(rawIpc);
-  HandleIpc.handleIpc(ipc);
-  const rpc = CreateRpc.createRpc(ipc);
-  messagePort.start();
-  return rpc;
-};
+  })
+  const ipc = IpcParentWithMessagePort.wrap(rawIpc)
+  HandleIpc.handleIpc(ipc)
+  const rpc = CreateRpc.createRpc(ipc)
+  messagePort.start()
+  return rpc
+}
--- packages/rpc/src/parts/TransferMessagePortRpcParent/TransferMessagePortRpcParent.ts
+++ packages/rpc/src/parts/TransferMessagePortRpcParent/TransferMessagePortRpcParent.ts
@@ -4,14 +4,17 @@ import * as PlainMessagePortRpc from '../PlainMessagePortRpc/PlainMessagePortRpc
 export const create = async ({
   commandMap,
   send,
+  isMessagePortOpen,
 }: {
   commandMap: any
   send: (port: MessagePort) => Promise<void>
+  isMessagePortOpen?: boolean
 }): Promise<Rpc> => {
   const { port1, port2 } = new MessageChannel()
   await send(port1)
   return PlainMessagePortRpc.create({
     commandMap,
     messagePort: port2,
+    isMessagePortOpen,
   })
 }
