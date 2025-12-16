c8519e7c71c719846bf8b6d3929b8c59384641f5
Le Vivilet <levivilet@pm.me>
2025-12-16 11:54:43 +0100

    feature: allow marking message port as already open or not (#235)
    * feature: allow marking message port as already open or not
    
    * lint

--- packages/rpc/src/parts/TransferMessagePortRpcParent/TransferMessagePortRpcParent.ts
+++ packages/rpc/src/parts/TransferMessagePortRpcParent/TransferMessagePortRpcParent.ts
@@ -3,15 +3,18 @@ import * as PlainMessagePortRpc from '../PlainMessagePortRpc/PlainMessagePortRpc
 
 export const create = async ({
   commandMap,
+  isMessagePortOpen,
   send,
 }: {
   commandMap: any
   send: (port: MessagePort) => Promise<void>
+  isMessagePortOpen?: boolean
 }): Promise<Rpc> => {
   const { port1, port2 } = new MessageChannel()
   await send(port1)
   return PlainMessagePortRpc.create({
     commandMap,
+    isMessagePortOpen,
     messagePort: port2,
   })
 }
