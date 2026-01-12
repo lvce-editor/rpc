2b1fb118155858454b7117800ad65408dc1e42cf
Le Vivilet <levivilet@pm.me>
2026-01-12 12:59:25 +0100

    feature: add lazy messageport rpc

--- packages/rpc/src/parts/Main/Main.ts
+++ packages/rpc/src/parts/Main/Main.ts
@@ -20,6 +20,7 @@ export * as WebSocketRpcParent from '../WebSocketRpcParent/WebSocketRpcParent.ts
 export * as WebSocketRpcParent2 from '../WebSocketRpcParent2/WebSocketRpcParent2.ts'
 export * as WebWorkerRpcClient from '../WebWorkerRpcClient/WebWorkerRpcClient.ts'
 export * as WebWorkerRpcClient2 from '../WebWorkerRpcClient2/WebWorkerRpcClient2.ts'
+export * as LazyTransferMessagePortRpcParent from '../LazyTransferMessagePortRpcParent/LazyTransferMessagePortRpcParent.ts'
 export * from '../CreateMockRpc/CreateMockRpc.ts'
 export * from '../CreateWebWorkerRpcClient/CreateWebWorkerRpcClient.ts'
 export * from '../MockWebSocketRpc/MockWebSocketRpc.ts'
