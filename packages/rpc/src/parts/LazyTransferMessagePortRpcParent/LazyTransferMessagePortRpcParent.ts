4080b09f9fcfbe34c6f0879dac3bcfec2e7fa17a
Le Vivilet <levivilet@pm.me>
2026-01-12 13:01:52 +0100

    feature: add lazy messageport rpc (#252)
    * feature: add lazy messageport rpc
    
    * fix

---
+++ packages/rpc/src/parts/LazyTransferMessagePortRpcParent/LazyTransferMessagePortRpcParent.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import type { Rpc } from '../Rpc/Rpc.ts'
+import { TransferMessagePortRpcParent } from '../Main/Main.ts'
+
+export const create = async ({
+  commandMap,
+  isMessagePortOpen,
+  send,
+}: {
+  commandMap: any
+  send: (port: MessagePort) => Promise<void>
+  isMessagePortOpen?: boolean
+}): Promise<Rpc> => {
+  let rpcPromise: Promise<Rpc> | undefined
+  const getOrCreate = (): Promise<Rpc> => {
+    if (!rpcPromise) {
+      rpcPromise = TransferMessagePortRpcParent.create({
+        commandMap,
+        isMessagePortOpen,
+        send,
+      })
+    }
+    return rpcPromise
+  }
+  return {
+    async dispose(): Promise<void> {
+      const rpc = await getOrCreate()
+      await rpc.dispose()
+    },
+    async invoke(method, ...params): Promise<any> {
+      const rpc = await getOrCreate()
+      return rpc.invoke(method, ...params)
+    },
+    async invokeAndTransfer(method, ...params): Promise<any> {
+      const rpc = await getOrCreate()
+      return rpc.invokeAndTransfer(method, ...params)
+    },
+    async send(method, ...params): Promise<void> {
+      const rpc = await getOrCreate()
+      rpc.send(method, ...params)
+    },
+  }
+}
