b703fc33a61338370bbda44e1343f1df12bfd0f9
Le Vivilet <levivilet@pm.me>
2026-02-13 11:27:00 +0100

    feature: don't bundle code for easier types and better treeshaking

--- packages/build/src/bundleJs.js
+++ packages/build/src/bundleJs.js
@@ -1,54 +1,11 @@
-import pluginTypeScript from '@babel/preset-typescript'
-import { babel } from '@rollup/plugin-babel'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { execa } from 'execa'
 import { join } from 'path'
-import { rollup } from 'rollup'
 import { root } from './root.js'
-
-/**
- * @type {import('rollup').RollupOptions}
- */
-const options = {
-  input: join(root, 'packages/rpc/src/index.ts'),
-  preserveEntrySignatures: 'strict',
-  treeshake: {
-    propertyReadSideEffects: false,
-  },
-  output: {
-    file: join(root, '.tmp/dist/dist/index.js'),
-    format: 'es',
-    freeze: false,
-    generatedCode: {
-      constBindings: true,
-      objectShorthand: true,
-    },
-  },
-  external: [
-    '@lvce-editor/assert',
-    '@lvce-editor/command',
-    '@lvce-editor/ipc',
-    '@lvce-editor/ipc',
-    '@lvce-editor/ipc/dist/browser.js',
-    '@lvce-editor/json-rpc',
-    '@lvce-editor/ripgrep',
-    '@lvce-editor/rpc',
-    '@lvce-editor/verror',
-    'electron',
-    'execa',
-    'ws',
-  ],
-  plugins: [
-    babel({
-      babelHelpers: 'bundled',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: [pluginTypeScript],
-    }),
-    nodeResolve(),
-  ],
-}
+import { cp } from 'fs/promises'
 
 export const bundleJs = async () => {
-  const input = await rollup(options)
-  // @ts-ignore
-  await input.write(options.output)
+  await execa(`npx`, ['tsc', '-b'], {
+    cwd: join(root, 'packages', 'rpc'),
+  })
+  await cp(join(root, 'packages', 'rpc', 'dist', 'src'), join(root, '.tmp', 'dist', 'dist'), { recursive: true })
 }
