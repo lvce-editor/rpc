d841a33c6bb098cb56b7f1c0793ad4df93633a4e
Le Vivilet <levivilet@pm.me>
2026-07-10 18:20:17 +0000

    fix: improve rpc connection error

--- packages/rpc/src/parts/WebSocketRpcParent/WebSocketRpcParent.ts
+++ packages/rpc/src/parts/WebSocketRpcParent/WebSocketRpcParent.ts
@@ -1,4 +1,5 @@
 import { IpcParentWithWebSocket } from '@lvce-editor/ipc'
+import { VError } from '@lvce-editor/verror'
 import type { Rpc } from '../Rpc/Rpc.ts'
 import * as Command from '../Command/Command.ts'
 import * as CreateRpc from '../CreateRpc/CreateRpc.ts'
@@ -7,9 +8,14 @@ import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 export const create = async ({ commandMap, webSocket }: { commandMap: any; webSocket: WebSocket }): Promise<Rpc> => {
   // TODO create a commandMap per rpc instance
   Command.register(commandMap)
-  const rawIpc = await IpcParentWithWebSocket.create({
-    webSocket,
-  })
+  let rawIpc: any
+  try {
+    rawIpc = await IpcParentWithWebSocket.create({
+      webSocket,
+    })
+  } catch (error) {
+    throw new VError(error, 'Failed to create rpc connection')
+  }
   const ipc = IpcParentWithWebSocket.wrap(rawIpc)
   HandleIpc.handleIpc(ipc)
   const rpc = CreateRpc.createRpc(ipc)
