5153d3afd40d84ba1ce69d20c19329ec15e3308d
Le Vivilet <levivilet@pm.me>
2026-07-23 11:03:03 +0200

    feature: notify when websocket rpc closes (#304)

--- packages/rpc/src/parts/WebSocketRpcParent2/WebSocketRpcParent2.ts
+++ packages/rpc/src/parts/WebSocketRpcParent2/WebSocketRpcParent2.ts
@@ -3,11 +3,22 @@ import * as GetWebSocketUrl from '../GetWebSocketUrl/GetWebSocketUrl.ts'
 import * as Location from '../Location/Location.ts'
 import * as WebSocketRpcParent from '../WebSocketRpcParent/WebSocketRpcParent.ts'
 
-export const create = async ({ commandMap, type }: { commandMap: any; type: string }): Promise<Rpc> => {
+export const create = async ({
+  commandMap,
+  onClose,
+  type,
+}: {
+  commandMap: any
+  onClose?: () => void
+  type: string
+}): Promise<Rpc> => {
   const host = Location.getHost()
   const protocol = Location.getProtocol()
   const wsUrl = GetWebSocketUrl.getWebSocketUrl(type, host, protocol)
   const webSocket = new WebSocket(wsUrl)
+  if (onClose) {
+    webSocket.addEventListener('close', onClose, { once: true })
+  }
   const rpc = await WebSocketRpcParent.create({
     commandMap,
     webSocket,
