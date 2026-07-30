cfa6a596206ac254c68fee5e009f65bab157a915
Le Vivilet <levivilet@pm.me>
2026-07-30 15:17:29 +0000

    feature: add knip

--- packages/rpc/src/parts/JsonRpcTypes/JsonRpcTypes.ts
+++ packages/rpc/src/parts/JsonRpcTypes/JsonRpcTypes.ts
@@ -5,31 +5,13 @@ export interface IJsonRpcRequest<TParams = unknown> {
   readonly params?: TParams
 }
 
-export interface IJsonRpcSuccessResponse<TResult = unknown> {
-  readonly id: number | string | null
-  readonly jsonrpc: '2.0'
-  readonly result: TResult
-}
-
-export interface IJsonRpcErrorResponse {
-  readonly error: JsonRpcError
-  readonly id: number | string | null
-  readonly jsonrpc: '2.0'
-}
-
-export interface JsonRpcError {
-  readonly code: number
-  readonly data?: unknown
-  readonly message: string
-}
-
 export interface JsonRpcEvent<TParams = unknown> {
   readonly jsonrpc: '2.0'
   readonly method: string
   readonly params: TParams
 }
 
-export interface Transport {
+interface Transport {
   send(message: unknown): void
   sendAndTransfer?(message: unknown): void
 }
@@ -37,17 +19,4 @@ export interface Transport {
 export interface IpcConnection extends Transport {
   send(message: unknown): void
   sendAndTransfer?(message: unknown): void
-}
-
-export interface JsonRpcRequestResult<T = unknown> {
-  message: IJsonRpcRequest
-  promise: Promise<T>
-}
-
-export interface ErrorResponseData {
-  readonly code?: number | string
-  readonly codeFrame?: string
-  readonly name?: string
-  readonly stack?: string
-  readonly type?: string
 }
--- packages/rpc/src/parts/UtilityProcessState/UtilityProcessState.ts
+++ packages/rpc/src/parts/UtilityProcessState/UtilityProcessState.ts
@@ -1,6 +1,6 @@
 import * as Assert from '@lvce-editor/assert'
 
-export const state = {
+const state = {
   all: Object.create(null),
 }
 
@@ -17,19 +17,4 @@ export const add = (pid: number, process: any, name: string): void => {
 export const remove = (pid: number): void => {
   Assert.number(pid)
   delete state.all[pid]
-}
-
-export const getAll = (): any => {
-  return Object.entries(state.all)
-}
-
-export const getByName = (name: string): any => {
-  for (const value of Object.values(state.all)) {
-    // @ts-ignore
-    if (value.name === name) {
-      // @ts-ignore
-      return value.process
-    }
-  }
-  return undefined
 }
