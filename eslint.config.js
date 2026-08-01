9c1e75b0c5da00d8b791807426e39797170160f2
Le Vivilet <levivilet@pm.me>
2026-08-01 14:47:02 -0700

    feature: update eslint dependencies (#316)

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
+export default defineConfig([
   ...config.default,
-  ...actions.default,
+  ...config.recommendedActions,
   ...config.recommendedNode,
   {
     files: ['**/*.ts'],
@@ -32,4 +32,4 @@ export default [
       'e2e/no-imports': 'off',
     },
   },
-]
+])
